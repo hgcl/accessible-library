4b5f36dd5d61a5eb10f945372663e334335c67c7
Clara Le <hey@clarale.com>
2025-01-08 15:43:41 +0700

    build: add storybook dependency

---
+++ .storybook/main.js
@@ -0,0 +1,18 @@
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  stories: [
+    "../src/stories/*.mdx",
+    "../src/stories/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+};
+export default config;
