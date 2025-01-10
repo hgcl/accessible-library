9e511f3345e5847aa29eb5e367017148549ae7f0
Clara Le <hey@clarale.com>
2025-01-10 12:03:10 +0700

    build: remove prop-types dependency to avoid problem with rollup

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,25 +1,15 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 const Button = ({ label, hierarchy, ...props }) => {
   return (
     <button
       type="button"
-      className={[`storybook-button--${hierarchy}`].join(" ")}
+      className={[`lib-button--${hierarchy}`].join(" ")}
       {...props}
     >
       {label}
     </button>
   );
-};
-
-Button.propTypes = {
-  /** Button hierarchy */
-  hierarchy: PropTypes.oneOf(["primary", "secondary", "tertiary"]),
-  /** Button contents */
-  label: PropTypes.string.isRequired,
-  /** Optional click handler */
-  onClick: PropTypes.func,
 };
 
 Button.defaultProps = {
